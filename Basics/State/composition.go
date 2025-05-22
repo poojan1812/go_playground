3e8ed4887a0731f0cadb72a9b1ef0a7e5123e891
poojan1812 <mehtapoojan1812@gmail.com>
2025-05-21 22:40:23 -0400

    Add method forwarding and struct embedding examples

--- Basics/State/composition.go
+++ Basics/State/composition.go
@@ -31,4 +31,20 @@ func main() {
 	// Specific detail can also be accessed
 	fmt.Printf("location for the branch: %v\n", branch_detail.bank.location)
 
+	// Method forwarding makes it convenient to use multiple methods.
+	// Go Supports this with Struct embedding concept. Specify a type without field name to embed it in a struct.
+	type report struct {
+		temperature
+		sol int
+		area
+	}
+	// Here, all the methods on temperature type are automatically accesible to report.
+	// Fields of inner structure is accesible to the outer structure.
+	// report.temperature.high can also be accessed through report.high ~ change in either of this is applicable to the other as it shares the same underlying value
+
+	// Go compiler reports `name collision` error if two or more types are embedded with the same method.
+	// Here, go does not know which method with which type to forward the call.
+	// To solve this, embed the main type with a method and manually forward methods to the embedded types.
+	// i.e return r.sol.days(s2) ~ here report type is manually forwarded to days method using sol type.
+
 }
